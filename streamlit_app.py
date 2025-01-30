d690a0765977244bc7ea0a70f512b822ddf52747
Sri Balaji Muruganandam <66082342+iSriBalaji@users.noreply.github.com>
2025-01-30 15:30:10 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-# import pip
-# pip.main(['install','tensorflow'])
 import tensorflow as tf
 import requests
 import os
@@ -8,8 +6,9 @@ from io import BytesIO
 from PIL import Image
 import numpy as np
 
-# Use the DRIVE_URL from st.secrets
+# Fetch URL from secrets for security
 # url = st.secrets['DRIVE_URL']  # The URL is now fetched from secrets
+
 # Modify the URL for direct download from Google Drive
 file_id = '1MDkq7qxw4Kj7_ResPR9XcgyRNrFjRLYU'  # Replace with your actual file ID
 url = f'https://drive.google.com/uc?export=download&id={file_id}'
@@ -20,19 +19,22 @@ response = requests.get(url)
 # Save the downloaded content as a .h5 file
 model_path = 'classifier_model.h5'
 
-import os
-
-model_path = 'classifier_model.h5'
-if not os.path.exists(model_path):
-    print(f"Model file does not exist at path: {model_path}")
+# Ensure the file was downloaded correctly
+if response.status_code == 200:
+    with open(model_path, 'wb') as file:
+        file.write(response.content)
 else:
-    print(f"Model file exists at path: {model_path}")
+    st.error(f"Failed to download the model, status code: {response.status_code}")
 
-with open(model_path, 'wb') as file:
-    file.write(response.content)
-
-# Load the model into TensorFlow
-model = tf.keras.models.load_model(model_path)
+# Check if model file exists
+if not os.path.exists(model_path):
+    st.error(f"Model file does not exist at path: {model_path}")
+else:
+    # Load the model into TensorFlow
+    try:
+        model = tf.keras.models.load_model(model_path)
+    except Exception as e:
+        st.error(f"Error loading model: {e}")
 
 # Define a function for image classification
 def classify_image(image):
